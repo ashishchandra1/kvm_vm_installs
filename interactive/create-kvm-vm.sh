72f42418d0451e9a985e6b8869af2529809f1b1e
Ashish Chandra <mail.ashishchandra@gmail.com>
2019-04-25 12:00:29 +0200

    Creating an interactive way of installing kvm vms

---
+++ interactive/create-kvm-vm.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z "$1" ];
+then
+  echo Specify a virtual-machine name.
+  exit 1
+fi
+
+NAME=$1
+RAM=$2
+VCPU=$3
+SIZE=$4
+
+sudo virt-install \
+--name $NAME \
+--ram $RAM \
+--disk path=/var/lib/libvirt/images/$1.qcow2,size=$SIZE \
+--vcpus $VCPU \
+--os-type linux \
+--os-variant linux \
+--network bridge:br0,model=virtio \
+--graphics none \
+--console pty,target_type=serial \
+--location 'http://cdn-fastly.deb.debian.org/debian/dists/testing/main/installer-amd64/' \
+--extra-args 'console=ttyS0,115300n8 serial'
